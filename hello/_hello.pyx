fc9dae7874fc87884a7390f3799dcb795a28b121
messense <messense@icloud.com>
2020-05-10 15:17:04 +0800

    Remove unneeded code

--- hello/_hello.pyx
+++ hello/_hello.pyx
@@ -10,11 +10,3 @@ cpdef dict hello_absl():
     with nogil:
       a_map = fn_return_flat_hash_map()
     return {it.first: it.second.decode() for it in a_map}
-
-cpdef void hello(str strArg):
-    "Prints back 'Hello <param>', for example example: hello.hello('you')"
-    print("Hello, {}!".format(strArg))
-
-cpdef long elevation():
-    "Returns elevation of Nevado Sajama."
-    return 21463L
--- test_hello_cython.py
+++ test_hello_cython.py
@@ -4,13 +4,3 @@ import hello
 def test_hello_absl():
     ret = hello.hello_absl()
     assert ret == {1: "one", 2: "two", 3: "three"}
-
-
-def test_hello(capfd):
-    hello.hello("World")
-    captured = capfd.readouterr()
-    assert captured.out == "Hello, World!\n"
-
-
-def test_elevation():
-    assert hello.elevation() == 21463
